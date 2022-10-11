dfd0d4aa361aef23356c5d39ffbed4ad8a5a99c0
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2022-10-11 15:23:58 +0200

    db update

--- db/bowties.sql
+++ db/bowties.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Vært: 127.0.0.1
--- Genereringstid: 10. 10 2022 kl. 21:56:27
--- Serverversion: 10.1.38-MariaDB
--- PHP-version: 7.3.4
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 11, 2022 at 01:23 PM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,76 +24,78 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur-dump for tabellen `products`
+-- Table structure for table `media`
 --
 
-CREATE TABLE `products` (
-  `products_id` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `type` int(11) NOT NULL,
-  `description` varchar(250) NOT NULL,
-  `color` int(11) DEFAULT NULL,
-  `price` decimal(10,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Data dump for tabellen `products`
---
-
-INSERT INTO `products` (`products_id`, `name`, `type`, `description`, `color`, `price`) VALUES
-(1, 'Solid Black', 1, 'A solid black bowtie', 1, '150');
+DROP TABLE IF EXISTS `media`;
+CREATE TABLE IF NOT EXISTS `media` (
+  `media_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) COLLATE utf8_bin NOT NULL,
+  `type` varchar(20) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`media_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
--- Struktur-dump for tabellen `users`
+-- Table structure for table `products`
 --
 
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `products_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `type` int(11) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `color` int(11) DEFAULT NULL,
+  `price` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`products_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`products_id`, `name`, `type`, `description`, `color`, `price`) VALUES
+(1, 'Solid Black', 1, 'A solid black bowtie', 1, '150'),
+(2, 'Solid Faded Pink', 5, 'A solid faded pink bowtie', 24, '150');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_photos`
+--
+
+DROP TABLE IF EXISTS `product_photos`;
+CREATE TABLE IF NOT EXISTS `product_photos` (
+  `products_id` int(11) NOT NULL,
+  `media_id` int(11) NOT NULL,
+  `order_nr` int(11) NOT NULL,
+  PRIMARY KEY (`products_id`,`media_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
-  `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
--- Data dump for tabellen `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `name`, `password`) VALUES
 (1, 'Niclas', '1234'),
-(2, 'Nic', '123');
-
---
--- Begrænsninger for dumpede tabeller
---
-
---
--- Indeks for tabel `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`products_id`);
-
---
--- Indeks for tabel `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- Brug ikke AUTO_INCREMENT for slettede tabeller
---
-
---
--- Tilføj AUTO_INCREMENT i tabel `products`
---
-ALTER TABLE `products`
-  MODIFY `products_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Tilføj AUTO_INCREMENT i tabel `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+(2, 'Nic', '123'),
+(3, 'Meep', 'Moop');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
