3b8757d4973a28635fce55ef71c674ba5092c690
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2022-10-11 16:46:45 +0200

    giant mess but on the right track

--- controllers/createUser.php
+++ controllers/createUser.php
@@ -1,35 +1,43 @@
 <?php
-    $connPath = "dbconn.php";
-    while(!file_exists($connPath)){
-        $connPath = "../$connPath";
+    $rootPath = "";
+    while(!file_exists($rootPath . "index.php")){
+        $rootPath = "../$rootPath";
     }
-    require $connPath;
+    require $rootPath . "dbconn.php";
+    require $rootPath . "models/users.php";
 
+    $Users = new Users();
+    echo $Users->createUser;
     
     $name = $_POST['name'];
     $password = $_POST['password'];
-    
-    $sql = "SELECT name FROM users WHERE name = '$name'"; /* <-- Model */
+
+    $sql = "SELECT name FROM users WHERE name = '$name'";
     $data = $pdo->query($sql)->fetchAll();
 
     $validName = true;
     if($data[0]){
         $validName = false;
-    }
-    if($data[0] == $currentName){
-        $validName = true;
+
+        if($data[0] == $currentName){ /* $currentName not defined */
+            $validName = true; 
+        }
     }
 
-    if($validName && $name != ""){
-        $sql = "INSERT INTO users (name, password) VALUES ('$name', '$password')"; /* <-- Model */
+    //if($validName && $name != ""){
+        /* $createUser->prepare($Users->createUser);
+        $createUser->bindParam(':name', $name, PDO::PARAM_STR);
+        $createUser->bindParam(':password', $password, PDO::PARAM_STR);
+        $createUser->execute(); */
+        echo "Meeeep";
+        /* $sql = "INSERT INTO users (name, password) VALUES ('$name', '$password')";
     
         $pdo->query($sql);
         
         $_SESSION['name'] = $name;
         $_SESSION['loggedin'] = "true";
         
-        header("Location: /$URL/");
-    }else{
-        header("Location: /$URL/views/login/signup.php?err=invalidname");
-    }
- 
+        header("Location: /$URL/"); */
+    //}else{
+        //header("Location: /$URL/views/login/signup.php?err=invalidname");
+    //}
--- models/users.php
+++ models/users.php
@@ -2,15 +2,15 @@
 
 class Users{
     //Basic Crud
-    public $createUser = "INSERT INTO users (name, password) VALUES ('$name', '$password')";
+    public $createUser = 'INSERT INTO users (name, password) VALUES (:name, :password)';
 
-    public $readAllUsers = "SELECT * FROM users";
+    public $readAllUsers = 'SELECT * FROM users';
 
-    public $updateUser = "UPDATE users SET name = '$name', password = '$name' WHERE 'user_id' = '$user_id'";
+    public $updateUser = 'UPDATE users SET name = :name, password = :password WHERE `user_id` = :user_id';
 
-    public $deleteUser = "DELETE FROM users WHERE 'user_id' = '$user_id'";
+    public $deleteUser = 'DELETE FROM users WHERE `user_id` = :user_id';
     
     //Advanced queries
-    public $checkIfUserExists = "SELECT name FROM users WHERE name = $name";
+    public $checkIfUserExists = 'SELECT name FROM users WHERE name = `$name`';
     
 }
