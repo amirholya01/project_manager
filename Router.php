05041dcd8664582e969db976514e485120d34d75
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2022-11-02 10:17:10 +0100

    Fixed error with search
    Double prepare statement

--- Router.php
+++ Router.php
@@ -2,20 +2,12 @@
 
 class Router{
     private $request;
+    public $pineapple = "pineapple";
     public function __construct($requestPara){ /* Auto run when file is loaded */
         $this->request = $requestPara;
     }
     public function get($route, $file){
         $uri = trim($this->request, "/");
-
-        /* $params = explode("?", $uri);
-        $params = $params[1];
-        
-        $params = explode("&", $params);
-        for($i = 0; $i < count($params); $i++){
-            $params[$i] = explode("=", $params[$i]);
-        } */
-
         $uri = explode("/", $uri);
 
         if($uri[0] == trim($route, "/")){
--- controllers/getUsersWithFilters.php
+++ controllers/getUsersWithFilters.php
@@ -5,14 +5,20 @@
     }
     require $rootPath . "models/users.php";
 
+    $sql = "SELECT * FROM users WHERE name LIKE :name ";
     $name = "%";
-    if( isset( $_GET['name'] ) ){
-        $name = "%" . $_GET['name'] . "%";
+    if( isset( $args[0] ) && $args[0] != ""){
+        $id = $args[0];
+        $getUsers = $pdo->prepare("SELECT * FROM users WHERE name LIKE :name AND user_id = :id");
+        $getUsers->bindParam(':id', $id);
+        echo $id;
+    }else{
+        $getUsers = $pdo->prepare($sql);
     }
-    
-
-    $getUsers = $pdo->prepare("SELECT * FROM users WHERE name LIKE :name;");
-    $getUsers->bindParam(':name', $name);
+    if ( isset( $args[1] ) ){
+        $name = "%" . $args[1] . "%";
+    }
+    $getUsers->bindParam(':name', $name);    
     $getUsers->execute();
     
     $data = $getUsers->fetchAll();
