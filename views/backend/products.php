e7df16f90ced69b4412695348f75ab06d11aac85
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2022-11-30 13:18:52 +0100

    removed unessesary dependancy from products

--- views/backend/products.php
+++ views/backend/products.php
@@ -13,8 +13,6 @@
     require_once $rootPath . "security/formSpam.php";
     require_once $rootPath . "security/stringSanitation.php";
 
-    require_once $rootPath . "controllers/imageUpload.php";
-
     require_once $rootPath . "controllers/createProduct.php";
     require_once $rootPath . "controllers/editProduct.php";
     require_once $rootPath . "controllers/deleteProduct.php";
