4fbd68bfe9fcab7f7bffdbcd3e1e63076acbe9c1
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2022-12-06 10:27:14 +0100

    updated temporary database

--- dev/db/bowties.sql
+++ dev/db/bowties.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Vært: 127.0.0.1:3306
--- Genereringstid: 20. 11 2022 kl. 12:28:11
+-- Genereringstid: 06. 12 2022 kl. 09:25:40
 -- Serverversion: 5.7.36
 -- PHP-version: 7.4.26
 
@@ -17,10 +17,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-
-DROP DATABASE IF EXISTS bowties;
-CREATE DATABASE bowties;
-USE bowties;
+--
+-- Database: `bowties`
+--
 
 -- --------------------------------------------------------
 
@@ -40,57 +39,8 @@ CREATE TABLE IF NOT EXISTS `assign_colors_to_products` (
 --
 
 INSERT INTO `assign_colors_to_products` (`product_id`, `color_id`) VALUES
-(1, 1),
-(1, 2),
-(2, 2),
-(11, 1),
-(18, 1),
-(18, 2),
-(28, 1),
-(28, 2),
-(29, 1),
-(29, 2),
-(30, 1),
-(30, 2),
-(31, 1),
-(31, 2),
-(32, 1),
-(32, 2),
-(33, 1),
-(33, 2),
-(34, 1),
-(34, 2),
-(35, 1),
-(35, 2),
-(36, 1),
-(36, 2),
-(37, 1),
-(37, 2),
-(38, 1),
-(38, 2),
-(39, 1),
-(39, 2),
-(40, 1),
-(40, 2),
-(41, 1),
-(41, 2),
-(42, 1),
-(42, 2),
-(43, 1),
-(43, 2),
-(50, 1),
-(50, 2),
-(51, 1),
-(51, 2),
-(55, 1),
-(55, 2),
-(57, 1),
-(57, 2),
-(58, 1),
-(58, 2),
-(59, 1),
-(59, 2),
-(60, 2);
+(2, 1),
+(2, 2);
 
 -- --------------------------------------------------------
 
@@ -101,7 +51,7 @@ INSERT INTO `assign_colors_to_products` (`product_id`, `color_id`) VALUES
 DROP TABLE IF EXISTS `assign_media_to_products`;
 CREATE TABLE IF NOT EXISTS `assign_media_to_products` (
   `product_id` int(11) NOT NULL,
-  `media_id` int(11) NOT NULL,
+  `media_id` varchar(250) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`product_id`,`media_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -110,8 +60,8 @@ CREATE TABLE IF NOT EXISTS `assign_media_to_products` (
 --
 
 INSERT INTO `assign_media_to_products` (`product_id`, `media_id`) VALUES
-(59, 0),
-(60, 0);
+(2, '6380d60fecb10.jpg'),
+(2, '6385ed6ccb78c.jpg');
 
 -- --------------------------------------------------------
 
@@ -123,7 +73,6 @@ DROP TABLE IF EXISTS `media`;
 CREATE TABLE IF NOT EXISTS `media` (
   `media_id` varchar(250) COLLATE utf8_bin NOT NULL,
   `name` varchar(50) COLLATE utf8_bin DEFAULT '',
-  `type` int(11) DEFAULT NULL,
   PRIMARY KEY (`media_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -131,10 +80,34 @@ CREATE TABLE IF NOT EXISTS `media` (
 -- Data dump for tabellen `media`
 --
 
-INSERT INTO `media` (`media_id`, `name`, `type`) VALUES
-('6374dc23a3c27.jpg', 'pineapple', NULL),
-('6374d14b8aebd.jpg', 'pineapple', NULL),
-('6374dd3bd666f.jpg', 'pineapple', NULL);
+INSERT INTO `media` (`media_id`, `name`) VALUES
+('6380d60fecb10.jpg', 'Test'),
+('7380d60fecb10.jpg', 'new image :3');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `news`
+--
+
+DROP TABLE IF EXISTS `news`;
+CREATE TABLE IF NOT EXISTS `news` (
+  `news_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) COLLATE utf8_bin NOT NULL,
+  `description` text COLLATE utf8_bin NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `media` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`news_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Data dump for tabellen `news`
+--
+
+INSERT INTO `news` (`news_id`, `title`, `description`, `time`, `media`) VALUES
+(1, 'Meeeeeeeeeep!', 'Meep meepi di moop moop meep!', '2022-12-02 11:19:07', NULL),
+(7, 'does dis wook', 'ireioieroiero erroiero eroneo dgoon nsoer', '2022-12-05 11:27:13', '6380d60fecb10.jpg'),
+(6, 'Test', 'i sure hope this works now', '2022-12-02 11:44:58', '6380d60fecb10.jpg');
 
 -- --------------------------------------------------------
 
@@ -149,28 +122,16 @@ CREATE TABLE IF NOT EXISTS `products` (
   `type` int(11) NOT NULL,
   `description` varchar(250) NOT NULL,
   `price` decimal(10,0) NOT NULL,
+  `primary_image` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`products_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Data dump for tabellen `products`
 --
 
-INSERT INTO `products` (`products_id`, `name`, `type`, `description`, `price`) VALUES
-(1, 'Solid Black', 1, 'A solid black bowtie', '150'),
-(2, 'Solid Faded Pink MORE Unicorns', 1, 'A solid faded pink bowtie', '150'),
-(9, 'sanity', 1, '3ijro', '32'),
-(11, 'sanitys', 1, 'wer', '213'),
-(16, 'iamEvil', 5, ':33', '1'),
-(18, 'meep', 1, 'now i have colors!', '123'),
-(27, 'im tottally new', 1, '1', '1'),
-(43, 'doubleNew', 1, 'meep', '10'),
-(55, 'plzz', 1, 'work', '23'),
-(56, 'testingsf', 1, 'ngi', '23'),
-(57, 'more testing', 1, '1234', '324'),
-(58, 'AAAAAAAARG', 1, 'me me scared', '123'),
-(59, 'plzz bind', 1, 'i hope this works!', '4390'),
-(60, 'plzzzz', 1, 'plzzzzzzzzzz', '23');
+INSERT INTO `products` (`products_id`, `name`, `type`, `description`, `price`, `primary_image`) VALUES
+(2, 'Solid Faded Pink MORE Unicorns', 1, 'A solid faded pink bowtie', '150', '7380d60fecb10.jpg');
 
 -- --------------------------------------------------------
 
@@ -192,20 +153,6 @@ CREATE TABLE IF NOT EXISTS `product_colors` (
 INSERT INTO `product_colors` (`id`, `color`) VALUES
 (1, 'Yellow'),
 (2, 'Green');
-
--- --------------------------------------------------------
-
---
--- Struktur-dump for tabellen `product_photos`
---
-
-DROP TABLE IF EXISTS `product_photos`;
-CREATE TABLE IF NOT EXISTS `product_photos` (
-  `products_id` int(11) NOT NULL,
-  `media_id` int(11) NOT NULL,
-  `order_nr` int(11) NOT NULL,
-  PRIMARY KEY (`products_id`,`media_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -247,7 +194,7 @@ CREATE TABLE IF NOT EXISTS `spam_prevention` (
 --
 
 INSERT INTO `spam_prevention` (`id`, `name`, `time`) VALUES
-(1, 'Niclas', '2022-11-16 11:53:15');
+(1, 'Niclas', '2022-12-05 09:27:13');
 
 -- --------------------------------------------------------
 
@@ -262,7 +209,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(100) NOT NULL,
   `role` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 --
 -- Data dump for tabellen `users`
@@ -272,15 +219,14 @@ INSERT INTO `users` (`user_id`, `name`, `password`, `role`) VALUES
 (1, 'Niclas', '1234', 1),
 (2, 'Nic', '123', 0),
 (3, 'Meep', 'Moop', 0),
-(5, 'feio', 'gdno', 0),
-(6, 'test1', 'test', 0),
 (7, 'test2IAmEditted', 'test', 0),
 (16, 'pineapple', '1324', 0),
 (17, 'newusertest', '1234', 0),
 (18, 'isthisworking', 'meep', 0),
 (19, 'roletest', '1234', 0),
 (20, 'roleShouldWork', '1234', 1),
-(21, 'Kim', '1234', 1);
+(21, 'Kim', '1234', 1),
+(22, 'HopeThisWorks', '1324', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
