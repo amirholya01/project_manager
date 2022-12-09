91a8ef175472b7043da66a30fc5df6595a8b1dce
behdin123 <behdin2010@gmail.com>
2022-12-09 12:21:35 +0100

    Admin panel - Fronpage css fixed

--- views/backend/frontpage.php
+++ views/backend/frontpage.php
@@ -18,7 +18,8 @@
     require_once $rootPath . "views/backend/partials/header.php";
 ?>
 
-<div class="wrapper">
+<div class="wrapper frontpage">
+
     <h2>
         Header
     </h2>
@@ -27,43 +28,47 @@
             Phone Number
         </h3>
         <p>The same as in contact</p>
-        <input type="text" name="phone">
-        <button type="submit">Edit</button>
+        <input class="input" type="text" name="phone">
+        <button class="height-button button submit" type="submit">Submit/Edit</button>
     </form>
+
     <form method="POST" action="/adminFrontpage"> <!--  <- Email  -->
         <h3>
             Email
         </h3>
         <p>The same as in contact</p>
-        <input type="text" name="email">
-        <button type="submit">Edit</button>
+        <input class="input" type="text" name="email">
+        <button class="height-button button submit" type="submit">Submit/Edit</button>
     </form>
+
     <form method="POST" action="/adminFrontpage"> <!--  <- Navigation  -->
         <h3>
             Navigation
         </h3>
-        <input type="text" name="navHome">
-        <input type="text" name="navProducts">
-        <input type="text" name="navAboutUs">
-        <input type="text" name="navContact">
-        <button type="submit">Edit</button>
+        <input class="input" type="text" name="navHome">
+        <input class="input" type="text" name="navProducts">
+        <input class="input" type="text" name="navAboutUs">
+        <input class="input" type="text" name="navContact">
+        <button class="height-button button submit" type="submit">Submit/Edit</button>
     </form>
     
+    <hr class="solid">
 
     <h2>
         Frontpage
     </h2>
     <!-- Main slider -->
     <!-- ^ should have create and delete -->
-    <form method="POST" action="/adminFrontpage"> <!--  <- About us  -->
+    <forms method="POST" action="/adminFrontpage"> <!--  <- About us  -->
         <h3>
             About us
         </h3>
         <p>This is the same one as on the about us page</p>
         <textarea name="aboutUs" id="" cols="60" rows="10"></textarea>
-        <button type="submit">Edit</button>
+        <button class="height-button button submit" type="submit">Submit/Edit</button>
     </form>
 
+    <hr class="solid">
 
     <h2>
         Products
@@ -72,18 +77,19 @@
         <h3>
             Sub title
         </h3>
-        <input type="text" name="productsSubtitle">
-        <button type="submit">Edit</button>
+        <input class="input" type="text" name="productsSubtitle">
+        <button class="height-button button submit" type="submit">Submit/Edit</button>
     </form>
     <form method="POST" action="/adminFrontpage"> <!--  <- Title  -->
         <h3>
             Title
         </h3>
-        <input type="text" name="productsTitle">
-        <button type="submit">Edit</button>
+        <input class="input" type="text" name="productsTitle">
+        <button class="height-button button submit" type="submit">Submit/Edit</button>
     </form>
     <!-- Categories  - Product types! -->
 
+    <hr class="solid">
 
     <h2>
         About us
@@ -92,15 +98,15 @@
         <h3>
             Sub title
         </h3>
-        <input type="text" name="aboutusSubtitle">
-        <button type="submit">Edit</button>
+        <input class="input" type="text" name="aboutusSubtitle">
+        <button class="height-button button submit" type="submit">Submit/Edit</button>
     </form>
     <form method="POST" action="/adminFrontpage"> <!--  <- Title  -->
         <h3>
             Title
         </h3>
-        <input type="text" name="aboutusTitle">
-        <button type="submit">Edit</button>
+        <input class="input" type="text" name="aboutusTitle">
+        <button class="height-button button submit" type="submit">Submit/Edit</button>
     </form>
     <!-- Descriptions -->
     <!-- ^ should have create and delete -->
@@ -110,9 +116,10 @@
         </h3>
         <p>This is the same one as on the frontpage</p>
         <textarea name="aboutUs" id="" cols="60" rows="10"></textarea>
-        <button type="submit">Edit</button>
+        <button class="height-button button submit" type="submit">Submit/Edit</button>
     </form>
 
+    <hr class="solid">
 
     <h2>
         Contact
@@ -121,25 +128,25 @@
         <h3>
             Sub title
         </h3>
-        <input type="text" name="contactSubtitle">
-        <button type="submit">Edit</button>
+        <input class="input" type="text" name="contactSubtitle">
+        <button class="height-button button submit" type="submit">Submit/Edit</button>
     </form>
     <form method="POST" action="/adminFrontpage"> <!--  <- Title  -->
         <h3>
             Title
         </h3>
-        <input type="text" name="contactTitle">
-        <button type="submit">Edit</button>
+        <input class="input" type="text" name="contactTitle">
+        <button class="height-button button submit" type="submit">Submit/Edit</button>
     </form>
     <form method="POST" action="/adminFrontpage"> <!--  <- Contact forms  -->
         <h3>
             Contact forms
         </h3>
-        <input type="text" name="conAddress" placeholder="Address">
-        <input type="text" name="conPhone" placeholder="Phone">
-        <input type="text" name="conEmail" placeholder="Email">
-        <input type="text" name="conFollow" placeholder="Follow us">
-        <button type="submit">Edit</button>
+        <input class="input" type="text" name="conAddress" placeholder="Address">
+        <input class="input" type="text" name="conPhone" placeholder="Phone">
+        <input class="input" type="text" name="conEmail" placeholder="Email">
+        <input class="input" type="text" name="conFollow" placeholder="Follow us">
+        <button class="height-button button submit" type="submit">Submit/Edit</button>
     </form>
     <br>
     <br>
