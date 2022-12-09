22746a9b38a041f339988be633f3313b6a0f260a
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2022-12-09 11:02:44 +0100

    edit frontpage layout

--- models/handlers/frontpageHandler.php
+++ models/handlers/frontpageHandler.php
@@ -15,11 +15,98 @@ class FrontpageHandler extends Frontpage{
         $this->db = $db;
     }
 
+    public function editPhoneNumber($phone){
+        $update = $this->db->prepare($this->);
+        $update->bindParam(":", $phone);
+        $update->execute();
+    }
+    
+    public function editEmail($email){
+        $update = $this->db->prepare($this->);
+        $update->bindParam(":", $email);
+        $update->execute();
+    }
+    
+    public function editAddress($address){
+        $update = $this->db->prepare($this->);
+        $update->bindParam(":", $address);
+        $update->execute();
+    }
+    
+    public function editFollowUs($text){
+        $update = $this->db->prepare($this->);
+        $update->bindParam(":", $text);
+        $update->execute();
+    }
+
+    public function editNav1($nav1){
+        $update = $this->db->prepare($this->);
+        $update->bindParam(":", $nav1);
+        $update->execute();
+    }
+    
+    public function editNav2($nav2){
+        $update = $this->db->prepare($this->);
+        $update->bindParam(":", $nav2);
+        $update->execute();
+    }
+    
+    public function editNav3($nav3){
+        $update = $this->db->prepare($this->);
+        $update->bindParam(":", $nav3);
+        $update->execute();
+    }
+    
+    public function editNav4($nav4){
+        $update = $this->db->prepare($this->);
+        $update->bindParam(":", $nav4);
+        $update->execute();
+    }
+
     public function editAboutUs($aboutUs){
         $update = $this->db->prepare($this->editAboutUsQuery);
         $update->bindParam(":text", $aboutUs);
         $update->execute();
     }
+
+    public function editProductsSubTitle($title){
+        $update = $this->db->prepare($this->);
+        $update->bindParam(":", $title);
+        $update->execute();
+    }
+    
+    public function editProductsTitle($title){
+        $update = $this->db->prepare($this->);
+        $update->bindParam(":", $title);
+        $update->execute();
+    }
+    
+
+    public function editAboutUsSubTitle($title){
+        $update = $this->db->prepare($this->);
+        $update->bindParam(":", $title);
+        $update->execute();
+    }
+    
+    public function editAboutUsTitle($title){
+        $update = $this->db->prepare($this->);
+        $update->bindParam(":", $title);
+        $update->execute();
+    }
+
+    
+    public function editContactSubTitle($title){
+        $update = $this->db->prepare($this->);
+        $update->bindParam(":", $title);
+        $update->execute();
+    }
+    
+    public function editContactTitle($title){
+        $update = $this->db->prepare($this->);
+        $update->bindParam(":", $title);
+        $update->execute();
+    }
+    
 }
 
 $FrontpageHandler = new FrontpageHandler($db);
--- views/backend/frontpage.php
+++ views/backend/frontpage.php
@@ -19,13 +19,135 @@
 ?>
 
 <div class="wrapper">
-    <form method="POST" action="/adminFrontpage">
+    <h2>
+        Header
+    </h2>
+    <form method="POST" action="/adminFrontpage"> <!--  <- Phone  -->
+        <h3>
+            Phone Number
+        </h3>
+        <p>The same as in contact</p>
+        <input type="text" name="phone">
+        <button type="submit">Edit</button>
+    </form>
+    <form method="POST" action="/adminFrontpage"> <!--  <- Email  -->
+        <h3>
+            Email
+        </h3>
+        <p>The same as in contact</p>
+        <input type="text" name="email">
+        <button type="submit">Edit</button>
+    </form>
+    <form method="POST" action="/adminFrontpage"> <!--  <- Navigation  -->
+        <h3>
+            Navigation
+        </h3>
+        <input type="text" name="navHome">
+        <input type="text" name="navProducts">
+        <input type="text" name="navAboutUs">
+        <input type="text" name="navContact">
+        <button type="submit">Edit</button>
+    </form>
+    
+
+    <h2>
+        Frontpage
+    </h2>
+    <!-- Main slider -->
+    <!-- ^ should have create and delete -->
+    <form method="POST" action="/adminFrontpage"> <!--  <- About us  -->
         <h3>
             About us
         </h3>
+        <p>This is the same one as on the about us page</p>
         <textarea name="aboutUs" id="" cols="60" rows="10"></textarea>
         <button type="submit">Edit</button>
     </form>
+
+
+    <h2>
+        Products
+    </h2>
+    <form method="POST" action="/adminFrontpage"> <!--  <- Sub title  -->
+        <h3>
+            Sub title
+        </h3>
+        <input type="text" name="productsSubtitle">
+        <button type="submit">Edit</button>
+    </form>
+    <form method="POST" action="/adminFrontpage"> <!--  <- Title  -->
+        <h3>
+            Title
+        </h3>
+        <input type="text" name="productsTitle">
+        <button type="submit">Edit</button>
+    </form>
+    <!-- Categories  - Product types! -->
+
+
+    <h2>
+        About us
+    </h2>
+    <form method="POST" action="/adminFrontpage"> <!--  <- Sub title  -->
+        <h3>
+            Sub title
+        </h3>
+        <input type="text" name="aboutusSubtitle">
+        <button type="submit">Edit</button>
+    </form>
+    <form method="POST" action="/adminFrontpage"> <!--  <- Title  -->
+        <h3>
+            Title
+        </h3>
+        <input type="text" name="aboutusTitle">
+        <button type="submit">Edit</button>
+    </form>
+    <!-- Descriptions -->
+    <!-- ^ should have create and delete -->
+    <form method="POST" action="/adminFrontpage"> <!--  <- About us  -->
+        <h3>
+            About us
+        </h3>
+        <p>This is the same one as on the frontpage</p>
+        <textarea name="aboutUs" id="" cols="60" rows="10"></textarea>
+        <button type="submit">Edit</button>
+    </form>
+
+
+    <h2>
+        Contact
+    </h2>
+    <form method="POST" action="/adminFrontpage"> <!--  <- Sub title  -->
+        <h3>
+            Sub title
+        </h3>
+        <input type="text" name="contactSubtitle">
+        <button type="submit">Edit</button>
+    </form>
+    <form method="POST" action="/adminFrontpage"> <!--  <- Title  -->
+        <h3>
+            Title
+        </h3>
+        <input type="text" name="contactTitle">
+        <button type="submit">Edit</button>
+    </form>
+    <form method="POST" action="/adminFrontpage"> <!--  <- Contact forms  -->
+        <h3>
+            Contact forms
+        </h3>
+        <input type="text" name="conAddress" placeholder="Address">
+        <input type="text" name="conPhone" placeholder="Phone">
+        <input type="text" name="conEmail" placeholder="Email">
+        <input type="text" name="conFollow" placeholder="Follow us">
+        <button type="submit">Edit</button>
+    </form>
+    <br>
+    <br>
+    <br>
+
+
+    <!-- FOOTER -->
+    
 </div>
 
 <?php 
