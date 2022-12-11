cd260d69b7d2937c87cc42b412377725f0ac72be
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2022-12-11 19:01:52 +0100

    added comment

--- controllers/purchase.php
+++ controllers/purchase.php
@@ -3,7 +3,7 @@
 /* Insert db */
 $PurchaseHandler->createOrder($_POST['country'],$_POST['firstname'],$_POST['lastname'],
 $_POST['address'],$_POST['appartment'],$_POST['city'],$_POST['state'],$_POST['postcode'],
-$_POST['email'],$_POST['phone'],$_POST['company']);
+$_POST['email'],$_POST['phone'],$_POST['company']);/* should probably have a payed bool */
 
 /* Checks if the data was inserted in the db */
 /* 
